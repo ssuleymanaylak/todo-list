c7ca276a79f925404615b7670344d400c3395c47
ssuleymanaylak <ssuleymanaylak@hotmail.com>
2023-12-31 08:31:02 +0300

    Pushed to github

--- migrations/Version20231229055341.php
+++ migrations/Version20231229055341.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20230927184230 extends AbstractMigration
+final class Version20231229055341 extends AbstractMigration
 {
     public function getDescription(): string
     {
@@ -20,13 +20,12 @@ final class Version20230927184230 extends AbstractMigration
     public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('CREATE TABLE task (id INT NOT NULL, title VARCHAR(100) NOT NULL, status VARCHAR(255) NOT NULL, created_at TIMESTAMP(0) WITHOUT TIME ZONE NOT NULL, PRIMARY KEY(id))');
+        
     }
 
     public function down(Schema $schema): void
     {
         // this down() migration is auto-generated, please modify it to your needs
         $this->addSql('CREATE SCHEMA public');
-        $this->addSql('DROP TABLE task');
     }
 }
